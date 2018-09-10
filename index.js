2669101671d7a6375cdc57fa62d190ae7a38d2b4
yoan ante <yoanante@gmail.com>
2018-09-10 00:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const app = "I don't do much."
 
-function moveDodgerLeft(){
-  document.addEventListener('keydown', function(e){
-    if (e.which === 37) {
-      var leftNumbers = dodger.style.left.replace('px', '')
-      var left = parseInt(leftNumbers, 10)
-      
-      dodger.style.left = `${left - 1}px`
-    }
-  })
-}
+document.addEventListener('keydown', function(e){
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
 
 function moveDodgerLeft(){
   var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  
+  if (left > 0){
+    dodger.style.left = `${left -1}px`
+  }
 
 }
